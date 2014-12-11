74ddb78f4bef6ab2ef0e3169a5127aa61c4d54f0
Kiko Beats <josefrancisco.verdu@gmail.com>
2014-12-11 12:19:14 +0100

    new build

--- dist/fn.partial.js
+++ dist/fn.partial.js
@@ -1,6 +1,6 @@
 /**
- * fn-partial - Pass a reference to a function with parameters (as a pointers).
- * @version v0.12.11
+ * fn-partial - Feel like a memory pointer. Pass a reference to a function.
+ * @version v0.12.11a
  * @link    https://github.com/Kikobeats/fn-partial
  * @license MIT
  */"use strict";module.exports=function(r){var t=Array.prototype.slice.call(arguments,1);return function(){var e;return e=t.concat(Array.prototype.slice.call(arguments)),r.apply(this,e)}};
